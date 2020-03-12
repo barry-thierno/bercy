e179c0a585d64628e54b6a60c9af7157d5b406f0
BARRY Thierno saidou <thiernosaidou.barry@axa.fr>
2020-03-12 09:29:39 +0100

    corrigé des deux premier exercice

--- src/bercy.test.ts
+++ src/bercy.test.ts
@@ -1,8 +1,23 @@
-import { great } from "./bercy";
+import { calculImpot } from "./bercy";
 
 describe("Bercy", () => {
-  it("should return hello word", () => {
-    const resp = great("test");
-    expect(resp).toBe("Hi, test!");
+  it("Pour Juliette Celibataire delcarant un salire inferieure ou egal à 10064€, elle doit payer un impot brut de  0€", () => {
+    const impotBrut = calculImpot(1000);
+    expect(impotBrut).toBe(0);
+  });
+
+  it("Pour Juliette Celibataire delcarant 20 000€, elle doit payer un impot brut de  1 111,04€", () => {
+    const impotBrut = calculImpot(20000.0);
+    expect(impotBrut).toEqual(1111.04);
+  });
+
+  it("Pour Romeo Celibataire delcarant 31 000€, elle doit payer un impot brut de  2 514€", () => {
+    const impotBrut = calculImpot(31000);
+    expect(impotBrut).toEqual(2514);
+  });
+
+  it("Pour Romeo Celibataire delcarant 31 000€, elle doit payer un impot brut de  2 514€", () => {
+    const impotBrut = calculImpot(41000);
+    expect(impotBrut).toEqual(5214);
   });
 });
--- src/bercy.ts
+++ src/bercy.ts
@@ -1,3 +1,35 @@
-export const great = (name: string) => {
-  return `Hi, ${name} !`;
+type Tranche = {
+  limite: number;
+  tauxImposition: number;
 };
+const tranches: Tranche[] = [
+  {
+    tauxImposition: 0.14,
+    limite: 10064
+  },
+  {
+    tauxImposition: 0.3,
+    limite: 27794
+  },
+  {
+    tauxImposition: 0.41,
+    limite: 74517
+  }
+];
+export const calculImpot = (salaireBrut: number) => {
+  const salaireImposable = salaireBrut - salaireBrut * 0.1;
+  return tranches.reduce((prev: number, curr) => {
+    // console.log(curr.limite);
+    if (salaireImposable >= curr.limite) {
+      const cal = round(
+        prev + (salaireImposable - curr.limite) * curr.tauxImposition
+      );
+      console.log(salaireBrut + " ", curr.limite, " r:", cal - prev);
+      return cal;
+    } else {
+      return prev;
+    }
+  }, 0);
+};
+
+const round = (n: number) => +n.toFixed(2);
