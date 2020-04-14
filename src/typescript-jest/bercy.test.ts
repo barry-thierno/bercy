dd620b69eb76ea079f8a574236c1b3e30d41a94f
BARRY Thierno saidou <thiernosaidou.barry@axa.fr>
2020-04-14 21:35:42 +0200

    ajout de la solution jest typescript

--- src/typescript-jest/bercy.test.ts
+++ src/typescript-jest/bercy.test.ts
@@ -1,28 +1,76 @@
-import { calculImpot } from "../typescript-cucumber/bercy";
+import { calculImpot } from "./bercy";
 
 describe("Bercy", () => {
   it("Pour Juliette Celibataire delcarant un salire inferieure ou egal à 10064€, elle doit payer un impot brut de  0€", () => {
-    const { impotBrute } = calculImpot(1000, 1);
+    const { impotBrute } = calculImpot(1000, 1, 0, "2019");
     expect(impotBrute).toBe("0");
   });
 
   it("Pour Juliette Celibataire delcarant 20 000€, elle doit payer un impot brut de  1 111,04€", () => {
-    const { impotBrute } = calculImpot(20000.0, 1);
+    const { impotBrute } = calculImpot(20000.0, 1, 0, "2019");
     expect(impotBrute).toEqual("1111");
   });
 
   it("Pour Romeo Celibataire delcarant 31 000€, elle doit payer un impot brut de  2 514€", () => {
-    const { impotBrute } = calculImpot(31000, 1);
+    const { impotBrute } = calculImpot(31000, 1, 0, "2019");
     expect(impotBrute).toEqual("2514");
   });
 
   it("Pour Romeo Celibataire delcarant 31 000€, elle doit payer un impot brut de  2 514€", () => {
-    const { impotBrute } = calculImpot(41000, 1);
+    const { impotBrute } = calculImpot(41000, 1, 0, "2019");
     expect(impotBrute).toEqual("5214");
   });
 
   it("Pour Romeo&Juliette marié delcarantion commune 61 000€ net de salaires (20 000 + 41 000), ils doivent payer un impot brut de  4 868,08 €", () => {
-    const { impotBrute } = calculImpot(61000, 2);
+    const { impotBrute } = calculImpot(61000, 2, 0, "2019");
     expect(impotBrute).toEqual("4868");
+  });
+
+  // Changement de taux et de tranches
+  it("Pour Juliette Celibataire delcarant un salire inferieure ou egal à 10064€, elle doit payer un impot brut de  0€", () => {
+    const { impotBrute } = calculImpot(1000, 1, 0, "2020");
+    expect(impotBrute).toBe("0");
+  });
+
+  it("Pour Juliette Celibataire delcarant 20 000€, elle doit payer un impot brut de  1 111,04€", () => {
+    const { impotBrute } = calculImpot(20000, 1, 0, "2020");
+    expect(impotBrute).toEqual("872");
+  });
+
+  it("Pour Romeo Celibataire delcarant 41 000€, elle doit payer un impot brut de  2 514€", () => {
+    const { impotBrute } = calculImpot(41000, 1, 0, "2020");
+    expect(impotBrute).toEqual("5087");
+  });
+
+  it("Pour Romeo&Juliette marié delcarantion commune 61 000€ net de salaires (20 000 + 41 000), ils doivent payer un impot brut de  4 868,08 €", () => {
+    const { impotBrute } = calculImpot(61000, 2, 0, "2020");
+    expect(impotBrute).toEqual("4504");
+  });
+
+  it("Pour Romeo&Juliette marié avec enfant delcaration commune 61 000€ net de salaires (20 000 + 41 000), ils doivent payer un impot brut de  4 868,08 € en 2019", () => {
+    const { impotBrute } = calculImpot(61000, 2, 1, "2019");
+    expect(impotBrute).toEqual("4163");
+  });
+
+  // Calcul le nombre de parts
+
+  it("1 enfant", () => {
+    const { nbPart } = calculImpot(20000, 0, 1, "2020");
+    expect(nbPart).toEqual(0.5);
+  });
+
+  it("2 enfants", () => {
+    const { nbPart } = calculImpot(41000, 0, 2, "2020");
+    expect(nbPart).toEqual(1);
+  });
+
+  it("3 enfants", () => {
+    const { nbPart } = calculImpot(61000, 0, 3, "2020");
+    expect(nbPart).toEqual(2);
+  });
+
+  it("4 enfants", () => {
+    const { nbPart } = calculImpot(61000, 0, 4, "2020");
+    expect(nbPart).toEqual(3);
   });
 });
--- src/typescript-jest/bercy.ts
+++ src/typescript-jest/bercy.ts
@@ -4,46 +4,106 @@ type Tranche = {
   tauxImposition: number;
 };
 
-const tranches: Tranche[] = [
+const tranche2019: Tranche[] = [
   {
     tauxImposition: 0.0,
     limiteInf: 0,
-    limiteSup: 10064
+    limiteSup: 10064,
   },
   {
     tauxImposition: 0.14,
     limiteInf: 10064,
-    limiteSup: 27794
+    limiteSup: 27794,
   },
   {
     tauxImposition: 0.3,
     limiteInf: 27794,
-    limiteSup: 74517
+    limiteSup: 74517,
   },
   {
     tauxImposition: 0.41,
     limiteInf: 74517,
-    limiteSup: 157806
+    limiteSup: 157806,
   },
   {
     tauxImposition: 0.45,
-    limiteInf: 157806
-  }
+    limiteInf: 157806,
+  },
 ];
+const tranche2020: Tranche[] = [
+  {
+    tauxImposition: 0.0,
+    limiteInf: 0,
+    limiteSup: 10064,
+  },
+  {
+    tauxImposition: 0.11,
+    limiteInf: 10064,
+    limiteSup: 25659,
+  },
+  {
+    tauxImposition: 0.3,
+    limiteInf: 25659,
+    limiteSup: 73369,
+  },
+  {
+    tauxImposition: 0.41,
+    limiteInf: 73369,
+    limiteSup: 157806,
+  },
+  {
+    tauxImposition: 0.45,
+    limiteInf: 157806,
+  },
+];
+export enum Annee {
+  ANNEE_2019 = "2019",
+  ANNEE_2020 = "2020",
+}
+type TranchesParAnnee = { [K in Annee]: Tranche[] };
+
+const anneesParTranche: TranchesParAnnee = {
+  [Annee.ANNEE_2019]: tranche2019,
+  [Annee.ANNEE_2020]: tranche2020,
+};
+
+/**
+ * Cacul le nombre de part en fonction du nombre d'adulte ou d'enfant
+ * @param nbAdulte
+ * @param nbEnfant
+ */
+const cauclerNbPart = (nbAdulte: number, nbEnfant: number) => {
+  let valeurPartEnfant = 0;
+  for (let index = 1; index <= nbEnfant; index++) {
+    if (index >= 3) {
+      valeurPartEnfant += 1;
+    } else {
+      valeurPartEnfant += 0.5;
+    }
+  }
+  return valeurPartEnfant + nbAdulte;
+};
 
 /**
  * Permet de calculer le montant d'impot en prennant en paramètre le salaire brut
  * @param salaireBrut
  */
-export const calculImpot = (salaireBrut: number, nbPart: number) => {
+export const calculImpot = (
+  salaireBrut: number,
+  nbAdult: number,
+  nbEnf: number,
+  annee: string
+) => {
+  const nbPart = cauclerNbPart(nbAdult, nbEnf);
   const salaireImposableParPart = (salaireBrut - salaireBrut * 0.1) / nbPart;
+  const tranches = anneesParTranche[annee];
   const impotBrute =
-    tranches.reduce((prev: number, curr) => {
+    tranches.reduce((prev: number, curr: Tranche) => {
       if (salaireImposableParPart >= curr.limiteSup) {
         // on paye plein impôt par rapport à la tranche
         return prev + (curr.limiteSup - curr.limiteInf) * curr.tauxImposition;
       } else if (salaireImposableParPart >= curr.limiteInf) {
-        // on paye un impo à la marge
+        // on paye un impôt à la marge
         return Math.trunc(
           prev +
             (salaireImposableParPart - curr.limiteInf) * curr.tauxImposition
@@ -54,8 +114,9 @@ export const calculImpot = (salaireBrut: number, nbPart: number) => {
       }
     }, 0) * nbPart;
   return {
-    impotBrute: `${impotBrute}`,
-    tauxImposition: `${toPercent(impotBrute / salaireBrut)}`
+    impotBrute: `${Math.round(impotBrute)}`,
+    tauxImposition: `${toPercent(impotBrute / salaireBrut)}`,
+    nbPart,
   };
 };
 
