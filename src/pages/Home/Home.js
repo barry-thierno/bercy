bed0f4e84b2e0a8a43c76b610b339035ea324867
Yannick <khalfouni.yannick@gmail.com>
2020-09-26 12:14:54 +0200

    feat(): init starter

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="container">
+    <h1 className="af-title--content">Home Page</h1>
+    <p>Hello World</p>
+  </div>
+);
+
+export default Home;
