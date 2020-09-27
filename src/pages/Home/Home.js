d0d09faaea5a909ba73baffd2c6782438f773dee
Khalfouni yannick <khalfouni.yannick@gmail.com>
2020-09-27 19:24:01 +0200

    feat(): add comment

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 const Home = () => (
-  <div className="container">
-    <h1 className="af-title--content">Home Page</h1>
-    <p>Hello World</p>
+  <div className="app">
+    {/********* Intégrer ici les composants Header, Name, et Title *********/}
+    <div className="app-content">
+      <div className="container">
+        <h1 className="af-title--content">Home Page</h1>
+        <p>Hello World</p>
+      </div>
+    </div>
+    {/********* Intégrer ici le composant Footer *********/}
   </div>
 );
 
