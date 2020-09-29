1f10acc04e013ea8e44a4e8cfab7ea109b195766
Yannick <khalfouni.yannick@gmail.com>
2020-09-30 01:00:33 +0200

    feat(): change name

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import '@axa-fr/react-toolkit-all/dist/style/bootstrap/grid.css';
 import '@axa-fr/react-toolkit-all/dist/style/bootstrap/reboot.css';
 import '@axa-fr/react-toolkit-core/dist/assets/fonts/icons/af-icons.css';
 import '@axa-fr/react-toolkit-all/dist/style/af-components.css';
-import Home from 'pages/Home/Home';
+import App from 'App';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
