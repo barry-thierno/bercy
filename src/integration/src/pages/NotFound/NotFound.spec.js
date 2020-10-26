e5b035492094570a417a5732d628d86b9d57c2e3
Yannick <khalfouni.yannick@gmail.com>
2020-10-26 22:45:36 +0100

    feat(): add home page example

--- src/integration/src/pages/NotFound/NotFound.spec.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import NotFound from './NotFound';
-
-it('1. Renders NotFound component without crashing', () => {
-  const { asFragment } = render(<NotFound title="test" />);
-  expect(asFragment()).toMatchSnapshot();
-});
