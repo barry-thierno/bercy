018433cb931979b4450de75e55a718d74dfbfb1f
BARRY Thierno saidou <thiernosaidou.barry@axa.fr>
2020-10-29 08:32:54 +0100

    feat: init chap 4

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@axa-fr/react-toolkit-all/dist/style/bootstrap/grid.css';
+import '@axa-fr/react-toolkit-all/dist/style/bootstrap/reboot.css';
+import '@axa-fr/react-toolkit-core/dist/assets/fonts/icons/af-icons.css';
+import '@axa-fr/react-toolkit-all/dist/style/af-components.css';
+import App from 'layout/App/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
