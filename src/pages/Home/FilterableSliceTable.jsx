569813213f508990bf02368878093084b207edfa
BARRY Thierno saidou <thiernosaidou.barry@axa.fr>
2020-10-29 09:25:32 +0100

    feat: solution 1

---
+++ src/pages/Home/FilterableSliceTable.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Table, SelectBase } from '@axa-fr/react-toolkit-all';
+
+export const FilterableSliceTable = () => {
+    return(
+      <section className="af-panel">
+        <header className="af-panel__header">
+         {/* SliceYearSelect */}
+        <SliceYearSelect/>
+        </header>
+        <div className="af-panel__content">
+          {/* SliceTable */}
+          <Table className="af-table">
+            {/* SliceTableHeader */}
+            <SliceTableHeader />
+            <Table.Body>
+              {/* SliceTableRow */}
+              <SliceTableRow lowBorn={0} highBorn={10064} rate= {0}/>
+              {/* SliceTableRow */}
+             <SliceTableRow lowBorn={10064} highBorn={25659} rate= {11}/>
+            </Table.Body>
+          </Table>
+        </div>
+      </section>
+    );
+}
+
+const SliceYearSelect = ()=> {
+  return (
+    <>
+      <span className="af-panel__title">Taux d'imposition par année</span>
+      <SelectBase
+        key="key"
+        name="name"
+        options={[
+          { value: '2019', label: '2019' },
+          { value: '2020', label: '2020' },
+        ]}
+        value="2020"
+      />
+    </>
+  )
+}
+const SliceTableHeader =() => {
+  return (
+    <Table.Header>
+      <Table.Tr>
+        <Table.Th>
+          <span className="af-table-th-content">Tranches</span>
+        </Table.Th>
+        <Table.Th>
+          <span className="af-table-th-content">
+            Taux d'imposition
+          </span>
+        </Table.Th>
+      </Table.Tr>
+    </Table.Header>
+  );
+}
+
+const SliceTableRow = ({lowBorn, highBorn, rate}) => {
+  return (
+  <Table.Tr>
+    <Table.Td>
+      <span className="af-table-body-content">
+        de {lowBorn} à {highBorn}
+      </span>
+    </Table.Td>
+    <Table.Td>
+      <b>{rate}%</b>
+    </Table.Td>
+  </Table.Tr>
+  )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import {
-  SelectBase,
-  Table,
-} from '@axa-fr/react-toolkit-all';
+import {FilterableSliceTable } from './FilterableSliceTable'
 import './Home.css';
 
 const Home = () => (
@@ -13,58 +10,7 @@ const Home = () => (
       </div>
 
       <aside className="tax-calculation_maximum-ceiling">
-        <section className="af-panel">
-          <header className="af-panel__header">
-            <span className="af-panel__title">Taux d'imposition par année</span>
-            <SelectBase
-              key="key"
-              name="name"
-              options={[
-                { value: '2019', label: '2019' },
-                { value: '2020', label: '2020' },
-              ]}
-              value="2020"
-            />
-          </header>
-          <div className="af-panel__content">
-            <Table className="af-table">
-              <Table.Header>
-                <Table.Tr>
-                  <Table.Th>
-                    <span className="af-table-th-content">Tranches</span>
-                  </Table.Th>
-                  <Table.Th>
-                    <span className="af-table-th-content">
-                      Taux d'imposition
-                    </span>
-                  </Table.Th>
-                </Table.Tr>
-              </Table.Header>
-              <Table.Body>
-                <Table.Tr>
-                  <Table.Td>
-                    <span className="af-table-body-content">
-                      jusqu'à 9 964€
-                    </span>
-                  </Table.Td>
-                  <Table.Td>
-                    <b>0%</b>
-                  </Table.Td>
-                </Table.Tr>
-                <Table.Tr>
-                  <Table.Td>
-                    <span className="af-table-body-content">
-                      de 9964 à 27519
-                    </span>
-                  </Table.Td>
-                  <Table.Td>
-                    <b>14%</b>
-                  </Table.Td>
-                </Table.Tr>
-              </Table.Body>
-            </Table>
-          </div>
-        </section>
+        <FilterableSliceTable />
       </aside>
     </div>
   </>
