8a3966ec032ce21fc021fd1a2c8e14ac4b30ca18
BARRY Thierno saidou <thiernosaidou.barry@axa.fr>
2020-11-12 11:24:53 +0100

    feat: fix architecture

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {FilterableSliceTable } from './FilterableSliceTable'
+import { FilterableSliceTable } from './FilterableSliceTable/FilterableSliceTable';
 import './Home.css';
 
 const Home = () => (
   <>
     <div className="tax-calculation">
       <div className="tax-calculation_content">
-      <div>Formulaire</div>
+        <div>Formulaire</div>
       </div>
 
       <aside className="tax-calculation_maximum-ceiling">
