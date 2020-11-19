7eb9fb8032d256903146760daf535173a610caa5
Yannick <khalfouni.yannick@gmail.com>
2020-11-19 11:56:46 +0100

    feat(): add fix for condeSandbox

--- src/layout/Footer/FooterLayout.jsx
+++ src/layout/Footer/FooterLayout.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Footer } from '@axa-fr/react-toolkit-all';
-import logo from '@axa-fr/react-toolkit-core/dist/assets/logo-axa.svg';
 import './FooterLayout.css';
 
 const FooterLayout = () => (
-  <Footer icon={logo} copyright={'© 2020 AXA All Rights Reserved'} />
+  <Footer icon="logo-axa.svg" copyright={'© 2020 AXA All Rights Reserved'} />
 );
 
 export default FooterLayout;
